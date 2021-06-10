1797ec18f3fad9f140591f94b42cc7fba072d011
Yassine <yassine.assouale@gmail.com>
2021-06-10 21:29:04 +0200

    Creating Configuration Classe

---
+++ src/main/java/crm/spring/rest/config/AppConfig.java
@@ -0,0 +1,98 @@
+package crm.spring.rest.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(basePackages = {"crm.spring.rest.repository"})
+@EnableTransactionManagement
+public class AppConfig {
+	
+	@Bean(name="entityManagerFactory") // JPA for test
+	@Profile("test")
+	public LocalContainerEntityManagerFactoryBean testEntityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(testDataSource());
+		em.setPackagesToScan("crm.spring.rest.model");
+		
+		// JpaVendor means implementer Jpa : specify wich jpa implementation we use
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(hibernatePropertiesForTest());
+		
+		return em;
+		
+	}
+	
+	@Bean // JPA for APP
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setPackagesToScan("crm.spring.rest.model");
+		
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(hibernateProperties());
+		
+		return em;
+		
+		
+	}
+	@Bean
+	private DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl("jdbc:postgresql://localhots:5432/formation");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("Y@ssine1983");
+		
+		return dataSource;
+	}
+	
+	@Bean(name="dataSource")
+	@Profile("test")
+	private DataSource testDataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.H2)
+				.addScript("classpath:data.sql")
+				.build();
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		final JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+		
+		return transactionManager;
+	}
+	
+	private Properties hibernatePropertiesForTest() {
+		Properties hibernateProperties = new Properties();
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "none");
+		hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		
+		return hibernateProperties;
+	}
+	
+	private Properties hibernateProperties() {
+		Properties hibernateProperties = new Properties();
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "none");
+		hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		return null;
+	}
+}
